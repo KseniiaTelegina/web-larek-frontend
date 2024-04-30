ef278fc4d1a5d7db6069eb4225b957ec12f8e008
Kseniia Telegina <ksusha2993@gmail.com>
2024-04-30 15:50:37 +0300

    обводка у кнопки работает, валидация еще нет

--- src/components/Order.ts
+++ src/components/Order.ts
@@ -1,88 +1,4 @@
-// import { Form } from "./Form"; 
-// import {IOrderForm} from "../types";
-// import {EventEmitter, IEvents} from "./base/events";
-// import { ensureElement, ensureAllElements } from "../utils/utils";
-// import { Component } from "./base/Component";
 
-// // interface IButtonPayment {
-// //     onClick: (tab: string) => void;
-// // }
-
-// export class Order extends Form<IOrderForm> {
-
-//     protected _button: HTMLElement;
-//     protected _payment: HTMLButtonElement;
-//     // protected _buttons: HTMLElement;
-
-//     constructor(protected container: HTMLFormElement, events: IEvents) {
-//         // constructor(protected container: HTMLFormElement, events: IEvents, actions?: IButtonPayment) {
-//         super(container, events);
-
-//         this._button = container.querySelector('.order__button');
-//         // this._buttons = Array.from(container.querySelectorAll('.button_alt'));
-//         this._payment = container.querySelector('.button_alt');
-
-//         if (this._button) {
-//             this._button.addEventListener('click', () => {
-//                 events.emit('contacts:open');
-//                 console.log('ку ку ку')
-//             });
-//         }
-
-//     // для переключения оплаты
-//     //     this._buttons.forEach(button => {
-//     //         button.addEventListener('click', () => {
-//     //             this._buttons.forEach(btn => btn.classList.remove('button_alt-active'));
-//     //             button.classList.add('button_alt-active');
-//     //         });
-//     //     });
-//      // this._buttons.forEach(button => {
-//         //     button.addEventListener('click', () => {
-//         //         actions?.onClick?.(button.name);
-//         //         console.log(button.name);
-//         //     });
-//         // })
-
-//         // if (this._payment) {
-//         //     this._payment.addEventListener('click', () => {
-//         //         if (this._payment.name === 'card') {
-//         //             // Передача параметра 'card' в событие 'payment:change'
-//         //             events.emit('payment:change');
-//         //             console.log('card');
-//         //         } else if (this._payment.name === 'cash') {
-//         //             // Передача параметра 'cash' в событие 'payment:change'
-//         //             events.emit('payment:change');
-//         //             console.log('cash');
-//         //         }
-//         //     });
-//         // }
-
-//         if (this._payment) {
-//             this._payment.addEventListener('click', () => {
-//                     events.emit('payment:change');
-//                     console.log('Способ оплаты выбран');
-//             });}
-//     }
-
-//     set payment (name: string) {
-//         if (this._payment === name) {
-//             if (this._payment)
-//             this.toggleClass(this._payment, 'button_alt-active', this._payment.name === name);
-//             this.setDisabled(this._payment, this._payment.name === name)
-//         }
-//     }
-    
-//     // set payment(name: string) {
-//     //     this._buttons.forEach(button => {
-//     //         this.toggleClass(button, 'button_alt-active', button.name === name);
-//     //         this.setDisabled(button, button.name === name)
-//     //     });
-//     // }
-
-//     set address(value: string) {
-//         (this.container.elements.namedItem('address') as HTMLInputElement).value = value;
-//     }
-// }
 
 import { Form } from "./Form"; 
 import {IOrderForm} from "../types";
@@ -90,8 +6,8 @@ import {EventEmitter, IEvents} from "./base/events";
 import { ensureElement, ensureAllElements } from "../utils/utils";
 import { Component } from "./base/Component";
 
-interface IOrderActions {
-    onClick: () => void;
+interface IButtonPayment {
+    onClick: (tab: string) => void;
 }
 
 export class Order extends Form<IOrderForm> {
@@ -103,8 +19,8 @@ export class Order extends Form<IOrderForm> {
                 constructor(protected container: HTMLFormElement, events: IEvents, actions?: IButtonPayment) {
         super(container, events);
 
-        this._button = container.querySelector('.button.order__button');
-        this._payment = Array.from(container.querySelectorAll('.button.button_alt'));
+        this._button = container.querySelector('.order__button');
+        this._payment = ensureAllElements<HTMLButtonElement>('.button_alt', container);
 
         if (this._button) {
             this._button.addEventListener('click', () => {
@@ -113,54 +29,30 @@ export class Order extends Form<IOrderForm> {
             });
         }
 
-        // // Добавляем валидацию для кнопки выбора оплаты
-        // this.container.addEventListener('change', () => {
-        //     const selectedPayment = this._payment.find(btn => btn.classList.contains('button_alt-active'));
-        //     const paymentValid = !!selectedPayment;
-        //     const addressValid = !!this.container.elements.namedItem('address').value;
-
-        //     this.valid = paymentValid && addressValid;
-        // });
-
     // для переключения оплаты
-        // this._payment.forEach(button => {
-        //     button.addEventListener('click', () => {
-        //         this._payment.forEach(btn => btn.classList.remove('button_alt-active'));
-        //         button.classList.add('button_alt-active');
-        //         console.log('Выбрана оплата', button.name)
 
-        //         //    // После изменения оплаты, проверяем валидность формы
-        //         //    const addressValid = !!this.container.elements.namedItem('address').value;
-        //         //    this.valid = addressValid;
-        //     });
-        // });
         this._payment.forEach(button => {
             button.addEventListener('click', () => {
                 actions?.onClick?.(button.name);
-
+                console.log(button.name);
+                this.selected = button.name;
             });
-        })
-
-
-                //    // После изменения оплаты, проверяем валидность формы
-                //    const addressValid = !!this.container.elements.namedItem('address').value;
-        //         //    this.valid = addressValid;
-        //     });
-        // });
-        
+        });
     }
 
 
-        set payment (name: string) {
-        if (this._payment === name) {
-            if (this._payment)
-            this.toggleClass(this._payment, 'button_alt-active', this._payment.name === name);
-            this.setDisabled(this._payment, this._payment.name === name)
-        }
+        set selected (name: string) {
+            this._payment.forEach(button => {
+                this.toggleClass(button, 'button_alt-active', button.name === name);
+        });
     }
-
 
     set address(value: string) {
         (this.container.elements.namedItem('address') as HTMLInputElement).value = value;
+    }
+
+    set payment(value: string) {
+        (this.container.elements.namedItem('card') as HTMLInputElement).value = value;
+        (this.container.elements.namedItem('cash') as HTMLInputElement).value = value;
     }
 }
